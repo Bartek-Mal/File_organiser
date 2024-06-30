2df123056acc161898361f538bd00290c38dd23b
Bartek-Mal <bartekgold8@gmail.com>
2024-06-30 17:05:28 +0200

    bugs fixed

--- file_organiser.py
+++ file_organiser.py
@@ -5,7 +5,6 @@ from colorama import Fore
 from send2trash import send2trash
 
 
-
 class Dir_management:
     def __init__(self):
         self.directory = None
@@ -40,39 +39,56 @@ class Dir_management:
 
                     if os.path.isfile(inner_filepath):
                         _, inner_file_extension = os.path.splitext(inner_filename)
-                        self.segregate(inner_file_extension, inner_dir)
+                        self.segregate(inner_file_extension, inner_filepath)
 
     def check_if_folder_exists(self):
         directories = [self.image_directory, self.pdf_directory, self.text_directory, self.spreadsheet_directory,
-                    self.video_directory, self.audio_directory, self.presentation_directory, self.cpp_directory, self.zip_directory]
+                       self.video_directory, self.audio_directory, self.presentation_directory, self.cpp_directory, self.zip_directory]
         for dir in directories:
             if not os.path.exists(dir):
                 os.mkdir(dir)
 
     def segregate(self, file_extension, filepath):
+        dest_directory = None
         if file_extension.lower() in ['.png', '.jpg']:
-            shutil.move(filepath, self.image_directory)
+            dest_directory = self.image_directory
         elif file_extension.lower() == '.pdf':
-            shutil.move(filepath, self.pdf_directory)
+            dest_directory = self.pdf_directory
         elif file_extension.lower() in ['.txt', '.doc', '.docx']:
-            shutil.move(filepath, self.text_directory)
+            dest_directory = self.text_directory
         elif file_extension.lower() in ['.xls', '.xlsx', '.csv']:
-            shutil.move(filepath, self.spreadsheet_directory)
+            dest_directory = self.spreadsheet_directory
         elif file_extension.lower() in ['.mp4', '.avi', '.mov']:
-            shutil.move(filepath, self.video_directory)
+            dest_directory = self.video_directory
         elif file_extension.lower() in ['.mp3', '.wav']:
-            shutil.move(filepath, self.audio_directory)
+            dest_directory = self.audio_directory
         elif file_extension.lower() in ['.ppt', '.pptx']:
-            shutil.move(filepath, self.presentation_directory)
+            dest_directory = self.presentation_directory
         elif file_extension.lower() in ['.cpp', '.h']:
-            shutil.move(filepath, self.cpp_directory)
+            dest_directory = self.cpp_directory
+        elif file_extension.lower() in ['.zip', '.rar']:
+            dest_directory = self.zip_directory
+
+        if dest_directory:
+            self.move_file(filepath, dest_directory)
+
+    def move_file(self, filepath, dest_directory):
+        filename = os.path.basename(filepath)
+        dest_filepath = os.path.join(dest_directory, filename)
+        if os.path.exists(dest_filepath):
+            base, ext = os.path.splitext(filename)
+            i = 1
+            while os.path.exists(dest_filepath):
+                new_filename = f"{base}({i}){ext}"
+                dest_filepath = os.path.join(dest_directory, new_filename)
+                i += 1
+        shutil.move(filepath, dest_filepath)
 
 
 dir_manager = Dir_management()
 
 running = True
-choices = IntEnum('Choices', {'Organise':1 , 'Display':2 , 'Delete':3, 'Move':4, 'Show':5, 'Quit':6})
-
+choices = IntEnum('Choices', {'Organise': 1, 'Display': 2, 'Delete': 3, 'Move': 4, 'Show': 5, 'Quit': 6})
 
 print(f"""{Fore.BLUE}
 ______ _     ___  ___
@@ -83,10 +99,9 @@ ______ _     ___  ___
 |___/ |_|_|  \_|  |_/\__,_|_| |_|\__,_|\__, |\___|_|
                                         __/ |
                                        |___/
-     {Fore.GREEN}This is an app i made during a weekend to
-       automate my directorymanagement tasks.
+     {Fore.GREEN}This is an app I made during a weekend to
+       automate my directory management tasks.
 {Fore.RESET}""")
-
 
 while running:
     print(f"{Fore.BLUE}1.Organise files\n2.Display files\n3.Delete file\n4.Move file\n5.Show presaved actions\n6.QUIT{Fore.RESET}")
@@ -111,16 +126,18 @@ while running:
         path = str(input("Directory path: "))
         os.chdir(path)
         for files in os.listdir(os.getcwd()):
-            print(f'{Fore.BLUE}--------------\n{Fore.RESET}',files)
+            print(f'{Fore.BLUE}--------------\n{Fore.RESET}', files)
     elif choice == choices.Delete:
         print(Fore.RED + """
                             !!WARNING!!
         Be aware that this action deletes the file permanently
         """ + Fore.RESET)
         path = str(input("Path: "))
+        original_directory = os.getcwd()
         os.chdir(path)
         file_to_delete = str(input("Which file would you like to delete?: "))
         send2trash(file_to_delete)
+        os.chdir(original_directory)
         print(f'''{Fore.CYAN}
         ______ _ _     ______     _      _           _ 
         |  ___(_) |    |  _  \   | |    | |         | |
@@ -134,9 +151,9 @@ while running:
         print("First input a path you want to move files from, then choose a file,\nat last input a path you want to move it to")
         from_path = str(input("From path: "))
         file = str(input("Enter file name: "))
-        file_path = os.path.join(from_path,file)
+        file_path = os.path.join(from_path, file)
         to_path = str(input("To path: "))
-        shutil.move(file_path,to_path)
+        shutil.move(file_path, to_path)
         print(f''' {Fore.CYAN}
         ______ _ _     ___  ___                   _ 
         |  ___(_) |    |  \/  |                  | |
@@ -145,7 +162,7 @@ while running:
         | |   | | |  __/ |  | | (_) \ V /  __/ (_| |
         \_|   |_|_|\___\_|  |_/\___/ \_/ \___|\__,_|
         {Fore.RESET}''')
-        
+
     elif choice == choices.Show:
         print(f"""{Fore.CYAN}
         ______         _____                      
@@ -166,17 +183,17 @@ while running:
         action = int(input(("1.Make a presave\n2.Show presaves\n3.Execute a presave\nChoice: ")))
         if action == 1:
             save = str(input("Make a presave (action path): "))
-            with open("pre_save.txt","a",encoding="UTF-8") as pre_save:
-                pre_save.write(save.replace(" ",";") + "\n")
+            with open("pre_save.txt", "a", encoding="UTF-8") as pre_save:
+                pre_save.write(save.replace(" ", ";") + "\n")
             print(f"Action {save} has been saved")
         elif action == 2:
             if os.path.exists("pre_save.txt"):
-                with open("pre_save.txt","r", encoding="UTF-8") as pre_save:
+                with open("pre_save.txt", "r", encoding="UTF-8") as pre_save:
                     content = pre_save.readlines()
                     if content:
                         print("Presaved actions:")
                         for line in content:
-                            print(line.strip().replace(" ",";"))
+                            print(line.strip().replace(" ", ";"))
                     else:
                         print("No presaved actions found")
         elif action == 3:
